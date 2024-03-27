77bc6c4058339bea9a049a9566252fb0d9c90ed3
rhc07 <rorieclarke@gmail.com>
2024-03-27 08:58:56 +0000

    update js compiled js file

--- tests/ivfRounds.test.js
+++ tests/ivfRounds.test.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const ivfRounds_1 = require("../src/ivfRounds");
 describe("Test generate", () => {
     const iVFRounds = new ivfRounds_1.IVFRounds;
-    it("should return list of string arrays with fizz and buzz", () => {
+    it("should return an array of five strings with fresh and frozen IVF", () => {
         expect(iVFRounds.generate(5)).toEqual([
             "FRESH",
             "FROZEN",
@@ -12,7 +12,7 @@ describe("Test generate", () => {
             "FROZEN+LIVE_BIRTH"
         ]);
     });
-    it("should return list of string arrays with fizzbuzz", () => {
+    it("should return an array of eighteen strings with fresh and frozen IVF", () => {
         expect(iVFRounds.generate(18)).toEqual([
             "FRESH",
             "FROZEN",
